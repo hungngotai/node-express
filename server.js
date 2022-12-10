1fd90dc3eea7c77dec69db052e5575a932220047
Ngo Tai Hung <ngotaihung@gmail.com>
2022-12-10 19:33:47 +0700

    Setting express with error handler (1)

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const UserRoute = require('./Routes/User.route')
+const createError = require('http-errors')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+  console.log('a:::', a)
+  res.send('Hello World!')
+})
+
+app.use('/user', UserRoute)
+
+app.use((req, res, next) => {
+  const error = createError.NotFound('This router does not exist.')
+  next(error)
+})
+
+app.use((err, req, res, next) => {
+  res.json({
+    status: err.status || 500,
+    message: err.message
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`)
+})
